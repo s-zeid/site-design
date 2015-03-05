32cab962e36f298075703fd35e4755eda088ea88
Scott Zeid <s@zeid.me>
2015-03-04 19:43:51 -0600

    [static/redirect.php] Allow wildcard redirects; some small things

--- static/redirect.php
+++ static/redirect.php
@@ -1,5 +1,10 @@
 <?php
 
+$request_uri = $_SERVER["REQUEST_URI"];
+function redirect($to) {
+ header("Location: $to");
+}
+
 $redirects_text = file_get_contents("_redirects");
 $redirects_text = preg_replace('/\s+[-=]>\s+/', ' => ', $redirects_text);
 $redirects_text = str_replace("\r", "\n", str_replace("\r\n", "\n", $redirects_text));
@@ -10,19 +15,25 @@ foreach ($redirect_lines as $line) {
  if (strpos($line, "#") !== 0) {
   $parts = explode(" => ", $line, 2);
   if (!empty($parts[0]) && !empty($parts[1]))
-   $redirects[$parts[0]] = $parts[1];
+   $redirects[rtrim($parts[0], "/")] = $parts[1];
  }
 }
 
 foreach ($redirects as $old => $new) {
- if ($_SERVER["REQUEST_URI"] == $old ||
-     $_SERVER["REQUEST_URI"] == $old."/") {
-  header("Location: $new");
+ if (strlen($old) >= 2 && substr($old, strlen($old) - 2) === "/*" &&
+     (strpos($request_uri, substr($old, 0, strlen($old) - 1)) === 0 ||
+      $request_uri == substr($old, 0, strlen($old) - 2))) {
+  redirect(rtrim($new, "/") . substr($request_uri, strlen($old) - 2));
+  exit();
+ }
+ if ($request_uri == $old ||
+     $request_uri == $old."/") {
+  redirect($new);
   exit();
  }
 }
 
-if ($_SERVER["REQUEST_URI"] != $_SERVER["SCRIPT_NAME"]) {
+if ($request_uri != $_SERVER["SCRIPT_NAME"]) {
  header("HTTP/1.0 404 Not Found");
  if (file_exists("404.html"))
   readfile("404.html");
