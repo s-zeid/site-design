dbecde67152e0b14eec6645f59a207063ea0f886
Scott Zeid <s@zeid.me>
2015-05-03 01:32:32 -0500

    [plugins/strip] Also strip leading and trailing whitespace

--- plugins/strip.rb
+++ plugins/strip.rb
@@ -4,17 +4,17 @@
 # Copyright (c) 2012 Aucor Oy
 # MIT License per <https://github.com/aucor/jekyll-plugins/blob/333794c/LICENCE>.
 
-# Replaces multiple newlines and whitespace 
-# between them with one newline
+# Modifications (c) 2015 Scott Zeid and released under the X11 license.
+
+# Replaces multiple newlines and whitespace between them with one newline
+# and then strips leading and trailing whitespace
 
 module Jekyll
-  class StripTag < Liquid::Block
-
-    def render(context)
-      super.gsub /\n\s*\n/, "\n"
-    end
-
+ class StripTag < Liquid::Block
+  def render(context)
+   super.gsub(/\n\s*\n/, "\n").strip()
   end
+ end
 end
 
 Liquid::Template.register_tag('strip', Jekyll::StripTag)
