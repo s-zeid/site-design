3a6b3ba5943ad0577fc029c17ae28706e913f037
Scott Zeid <s@zeid.me>
2015-05-03 01:33:37 -0500

    [plugins/strip] Add `lstrip`, `rstrip`, and `lrstrip` tags that only strip leading and/or trailing whitespace

--- plugins/strip.rb
+++ plugins/strip.rb
@@ -6,15 +6,37 @@
 
 # Modifications (c) 2015 Scott Zeid and released under the X11 license.
 
-# Replaces multiple newlines and whitespace between them with one newline
-# and then strips leading and trailing whitespace
+# Various whitespace-stripping block tags
 
 module Jekyll
  class StripTag < Liquid::Block
+  # Replaces multiple newlines and whitespace between them with one newline
+  # and then strips leading and trailing whitespace
   def render(context)
    super.gsub(/\n\s*\n/, "\n").strip()
+  end
+ end
+ class LStripTag < Liquid::Block
+  # Strips leading whitespace
+  def render(context)
+   super.lstrip()
+  end
+ end
+ class RStripTag < Liquid::Block
+  # Strips trailing whitespace
+  def render(context)
+   super.rstrip()
+  end
+ end
+ class LRStripTag < Liquid::Block
+  # Strips leading and trailing whitespace
+  def render(context)
+   super.strip()
   end
  end
 end
 
 Liquid::Template.register_tag('strip', Jekyll::StripTag)
+Liquid::Template.register_tag('lstrip', Jekyll::LStripTag)
+Liquid::Template.register_tag('rstrip', Jekyll::RStripTag)
+Liquid::Template.register_tag('lrstrip', Jekyll::LRStripTag)
