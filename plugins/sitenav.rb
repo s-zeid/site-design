e75d80c73f0e95f2e16b603f58ee94df5e42890b
Scott Zeid <s@zeid.me>
2015-05-07 00:13:23 -0500

    [plugins/sitenav] update copyright year

--- plugins/sitenav.rb
+++ plugins/sitenav.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2012-2014 Scott Zeid
+# Copyright (C) 2012-2015 Scott Zeid
 # http://code.s.zeid.me/site-design
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
