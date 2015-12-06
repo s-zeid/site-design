93551a88188055153dfe2bbcbe79f863f1487cc8
Scott Zeid <s@zeid.me>
2015-12-05 18:47:46 -0600

    change more references to code.s.zeid.me to use HTTPS

--- plugins/lsb_release.rb
+++ plugins/lsb_release.rb
@@ -1,5 +1,5 @@
 # Copyright (C) 2012-2014 Scott Zeid
-# http://code.s.zeid.me/site-design
+# https://code.s.zeid.me/site-design
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
